ee26396c3e94e05a11224efdef460ac04899985e
Ken Watson <furman87@gmail.com>
2019-05-06 20:46:51 -0400

    More app stuff added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Texting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
